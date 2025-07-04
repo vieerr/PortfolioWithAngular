9c54d0438980f50b6f45994e36fe111162ceedb4
vieerr <olivier_paspuel@hotmail.com>
2025-07-04 09:59:50 -0500

    feat: responsive support

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { AccordionItem } from './components/accordion-item/accordion-item';
 import { MeTab } from './components/Tabs/me-tab/me-tab';
 import { ProjectsTab } from "./components/Tabs/projects-tab/projects-tab";
 import { SkillsTab } from './components/Tabs/skills-tab/skills-tab';
@@ -15,3 +14,4 @@ import { SkillsTab } from './components/Tabs/skills-tab/skills-tab';
 export class App {
   currentTab = signal('projects');
 }
+
